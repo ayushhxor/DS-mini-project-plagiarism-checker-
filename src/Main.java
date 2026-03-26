d25b3f98a6e9ae573485e6708fbaada2d9b11186
Ayush Tripathi <ayushhtripathi1226@gmail.com>
2026-03-26 19:24:26 +0530

    Initialized project structure with skeleton classes

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Plagiarism Detector Started");
+
+        // TODO: Read files
+        // TODO: Process text
+        // TODO: Compare documents
+        // TODO: Display result
+    }
+}
