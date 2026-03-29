96264b4e418da0e6a0bd43099431e0c33fac062f
Ayush Tripathi <ayushhtripathi1226@gmail.com>
2026-03-29 17:11:02 +0530

    Cleaned and finalized Main class

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,21 @@ import comparison.SimilarityCalculator;
 import java.util.Scanner;
 
 public class Main {
+
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String text1, text2;
-        System.out.println("Enter the database text: ");
-        text1 = sc.nextLine();
-        System.out.println("Enter the input text: ");
-        text2 = sc.nextLine();
 
-        double result = SimilarityCalculator.calculateSimilarity(text1, text2);
-
-        System.out.println("Similarity: " + result + "%");
         System.out.println("Plagiarism Detector Started");
 
-        // TODO: Read files
-        // TODO: Process text
-        // TODO: Compare documents
-        // TODO: Display result
+        System.out.println("Enter the database text: ");
+        String databaseText = sc.nextLine();
+
+        System.out.println("Enter the input text: ");
+        String inputText = sc.nextLine();
+
+        double result = SimilarityCalculator.calculateSimilarity(databaseText, inputText);
+
+        System.out.println("Similarity: " + result + "%");
     }
 }
